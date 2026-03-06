37a3fe05dabdf1ee8db5c6addcf0df7988b73277
PMT201 <PMT201@PMT3037>
2026-03-06 10:41:54 +0700

    buoi 1

--- kythuatlaptrinh/buoi 1/buoi 1.cpp
+++ kythuatlaptrinh/buoi 1/buoi 1.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
+struct sinhvien {
+	string hoten;
+	int tuoi;
+	bool gioitinh;
+	string diachi;
+};
 int main() {
-	vector <float> diemthi;
+	sinhvien d;
+	d.hoten = "d4c";
+	d.tuoi = 36;
+	d.gioitinh = true;
+	d.diachi = "america";
+	
+	sinhvien c;
+	c.hoten = "nguyen van c";
+	c.tuoi = 29;
+	vector<sinhvien> mang;
+	mang.push_back(d);
+	mang.push_back(c);
+	for (int i = 0; i < mang.size(); i++) {
+		cout << "sinh vien" << (i + 1) << endl;
+		cout << "\t+ Ho ten:" << mang[i].hoten << endl;
+		cout << "\t+ tuoi:" << mang[i].tuoi << endl;
+	}
+	/*int a = 5;
+	int* p = &a;
+	a += 6;
+	a = *p + 2;
+	cout << *p;*/
+
+	/*vector <float> diemthi;
 	diemthi.push_back(5.5);
 	diemthi.push_back(6.5);
 	diemthi.push_back(6.7);
 	diemthi.pop_back();
 	for (int i = 0; i < diemthi.size(); i++) {
 		cout << diemthi[i] << endl;
-	}
+	}*/
 }
