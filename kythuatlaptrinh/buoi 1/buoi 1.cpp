a2dc96e21a9cb2659b83e82c3887d34392dfb93c
PMT201 <PMT201@PMT3037>
2026-03-06 09:10:21 +0700

    buoi 1

--- kythuatlaptrinh/buoi 1/buoi 1.cpp
+++ kythuatlaptrinh/buoi 1/buoi 1.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-
+	vector <float> diemthi;
+	diemthi.push_back(5.5);
+	diemthi.push_back(6.5);
+	diemthi.push_back(6.7);
+	diemthi.pop_back();
+	for (int i = 0; i < diemthi.size(); i++) {
+		cout << diemthi[i] << endl;
+	}
 }
