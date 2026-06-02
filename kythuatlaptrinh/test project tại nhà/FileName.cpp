ed54968ab2cb68c076bba8a1af07fff635b9b269
PC 2026 <PC 2026@SVPRO-20260415Q>
2026-06-02 23:59:55 +0700

    mink

--- kythuatlaptrinh/test project tại nhà/FileName.cpp
+++ kythuatlaptrinh/test project tại nhà/FileName.cpp
@@ -18,6 +18,106 @@
 //}
 #include <iostream>
 #include <string.h>
+using namespace std;
 struct Book {
+	int id;
+	char name[50];
+	double price;
+
+	friend istream& operator>>(istream& in, Book& m) { //tiếp thu thông tin sách 
+		cout << "Book information: " << endl;
+		cout << "\t+ ID: ";
+		in >> m.id;
+		cout << "/t+ Name: ";
+		in.ignore();
+		in.getline(m.name, 50);
+		cout << "\t_ Price: ";
+		in >> m.price;
+		return in;
+	}
+	friend ostream& operator<<(ostream& out, const Book& m) { //in thông tin sách đã tiếp thu trước đó
+		out << "Book information: " << endl;
+		out << "\t+ ID:" << m.id << endl;
+		out << "\t+ Name:" << m.name << endl;
+		out << "\t+ Price:" << m.price << endl;
+		return out;
+	}
 
 };
+struct node {
+	Book data;
+	node* next;
+};
+node* head = NULL;
+bool sorted = false;
+
+node* createnode(Book m) {  //tạo một cuốn sách mới 
+	node* newnode = new node();
+	newnode->data = m;  //data chuyển vào m để lưu thông tin sách
+	newnode->next = NULL; //data next mà k tồn tại thì trả lại cuốn sách cho nó thành head
+	return newnode;
+}
+void addbook(Book m) {
+	node* newnode = createnode(m);
+	if (head == NULL) {
+		head = newnode;
+		sorted = false;
+	}
+
+	node* temp = head;
+	while (temp->next != NULL) {
+		temp = temp->next;
+
+	}
+	temp->next = newnode;
+
+
+
+}
+void show() {
+	if (head == NULL) {
+		cout << " NO books available" << endl;
+		return;
+	}
+	node* temp = head;
+	while (temp != NULL) {
+		cout << "Book [ " << temp->data.id << " ] " << endl;
+		cout << temp->data;
+		temp = temp->next;
+
+
+	}
+
+}
+void sort() {
+	if (head == NULL || head->next == NULL) {
+		cout << " not enough books to sort" << endl;
+		return;
+	}
+	for (node* current = head; current->next != NULL; current = current->next) {
+		for (node* next = current->next; next != NULL; next = next->next) {
+			if (next->data.id > current->data.id) {
+				Book temp = current->data;
+				current->data = next->data;
+				next->data = temp;
+
+			}
+		}
+	}
+	cout << " sach da duoc sap xep" << endl;
+}
+void find() {
+	node* temp = head; 
+	bool found = false ;
+	while (temp != NULL) {
+		if (temp-> data.id == Bookid) {
+			cout << "data cua sach: " << endl;
+			cout << temp->data;
+			found = true;
+		}
+		temp = temp->next;
+	}
+	if (!found) {
+		cout << " khong tim thay sach can tim" << endl;
+	}
+}
